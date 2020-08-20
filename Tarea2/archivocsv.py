9f2b7cf1d390af18cce462eb6592069be939f79c
NeryBarrientos201807086 <3386309102008@ingenieria.usac.edu.gt>
2020-08-20 17:50:51 -0600

    print tipo de archivo

--- Tarea2/archivocsv.py
+++ Tarea2/archivocsv.py
@@ -2,4 +2,6 @@ with open('datos1.csv', 'r') as mi_archivocsv:
     mostrarlineas = mi_archivocsv.read().splitlines()
     for li in mostrarlineas:
         separados = li.split(',')
+        print ("El tipo del archivo es")
+        print (type(mostrarlineas))
         print (separados)
--- Tarea2/archivojson.py
+++ Tarea2/archivojson.py
@@ -2,7 +2,7 @@ import json
 
 with open('datos.json') as mi_Archivo:
     datos = json.loads(mi_Archivo.read())
-print ("El tipo de la lista es:")
+print ("El tipo del archivo es:")
 print(type(datos))
 #print(datos)
 for element in datos:
