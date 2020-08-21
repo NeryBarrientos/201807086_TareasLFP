ab5ddd8590a574243d08dcf68e3606c240c7c393
NeryBarrientos <68959243+NeryBarrientos@users.noreply.github.com>
2020-08-20 22:03:29 -0600

    Delete archivojson.py

--- Tarea2/archivojson.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('datos.json') as mi_Archivo:
-    datos = json.loads(mi_Archivo.read())
-print ("El tipo del archivo es:")
-print(type(datos))
-#print(datos)
-for element in datos:
-    print(element)
