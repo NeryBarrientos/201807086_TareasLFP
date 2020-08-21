e93770a45cb2353e65f58011a3263db4eaf47930
NeryBarrientos <68959243+NeryBarrientos@users.noreply.github.com>
2020-08-20 22:03:18 -0600

    Delete archivocsv.py

--- Tarea2/archivocsv.py
+++
@@ -1,7 +0,0 @@
-with open('datos1.csv', 'r') as mi_archivocsv:
-    mostrarlineas = mi_archivocsv.read().splitlines()
-    print ("El tipo del archivo es")
-    print (type(mostrarlineas))
-    for li in mostrarlineas:
-        separados = li.split(',')
-        print (separados)
